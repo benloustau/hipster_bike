d2ae1d74f567a138f414c3bb2635252932e218dc
Alexander Handler <alexjhandler@gmail.com>
2014-09-29 16:56:51 -0400

    add instance var for title

--- main.rb
+++ main.rb
@@ -2,17 +2,21 @@ require 'sinatra'
 require 'mandrill'
 
 get '/home' do
+	@title = "Hipster Bike"
 	erb :home
 end
 get '/about_us' do 
+	@title = "About Us"
 	erb :about_us
 end
 
 get '/contact_us' do 
+	@title = "Contact"
 	erb :contact_us
 end
 
 get '/rent_now' do 
+	@title = "Rent"
 	erb :rent_now
 end
 
